59108d3439bb25f424d0e6739aab7729a349c675
kpiega <kamil.piega@gmail.com>
2017-06-30 18:23:26 +0200

    inject presenter

--- app/src/main/java/team/deprecated/easyweather/activity/MainModule.kt
+++ app/src/main/java/team/deprecated/easyweather/activity/MainModule.kt
@@ -1,12 +1,16 @@
-//package team.deprecated.easyweather.activity
-//
-//import dagger.Binds
-//import dagger.Module
-//import dagger.android.support.DaggerAppCompatActivity
-//import team.deprecated.easyweather.di.scope.ActivityScope
-//
-//@Module
-//abstract class MainModule {
-//    @Binds
-//    abstract fun provideActivity(activity : MainActivity) : DaggerAppCompatActivity
-//}
+package team.deprecated.easyweather.activity
+
+import dagger.Binds
+import dagger.Module
+import team.deprecated.easyweather.activity.mvp.MainPresenter
+import team.deprecated.easyweather.activity.mvp.MainPresenterImpl
+
+@Module
+abstract class MainModule {
+
+    @Binds
+    abstract fun provideActivity(activity : MainActivity): MainActivity
+
+    @Binds
+    abstract fun providePresenter(presenter: MainPresenterImpl): MainPresenter
+}
--- app/src/main/java/team/deprecated/easyweather/di/ActivityBindingModule.kt
+++ app/src/main/java/team/deprecated/easyweather/di/ActivityBindingModule.kt
@@ -3,14 +3,17 @@ package team.deprecated.easyweather.di
 import dagger.Module
 import dagger.android.ContributesAndroidInjector
 import team.deprecated.easyweather.activity.MainActivity
+import team.deprecated.easyweather.activity.MainModule
 //import team.deprecated.easyweather.activity.MainModule
 import team.deprecated.easyweather.di.scope.ActivityScope
 
 @Module
 abstract class ActivityBindingModule {
 
-    @ContributesAndroidInjector
     @ActivityScope
+    @ContributesAndroidInjector(modules = arrayOf(
+            MainModule::class
+    ))
     abstract fun bindMainActivity(): MainActivity
 
 }
