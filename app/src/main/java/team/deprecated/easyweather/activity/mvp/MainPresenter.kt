3cae5f72310743232bb11940951f468ccd72a0c0
kpiega <kamil.piega@gmail.com>
2017-06-30 17:29:37 +0200

    fix merge

--- app/src/main/java/team/deprecated/easyweather/activity/mvp/MainPresenter.kt
+++ app/src/main/java/team/deprecated/easyweather/activity/mvp/MainPresenter.kt
@@ -2,10 +2,6 @@ package team.deprecated.easyweather.activity.mvp
 
 import com.hannesdorfmann.mosby3.mvp.MvpPresenter
 
-/**
- * Created by kpiega on 29.06.2017.
- */
-
 interface MainPresenter : MvpPresenter<MainView> {
 
 }
--- app/src/main/java/team/deprecated/easyweather/activity/mvp/MainPresenterImpl.kt
+++ app/src/main/java/team/deprecated/easyweather/activity/mvp/MainPresenterImpl.kt
@@ -1,8 +1,5 @@
 package team.deprecated.easyweather.activity.mvp
 
-/**
- * Created by kpiega on 29.06.2017.
- */
 
 class MainPresenterImpl: MainPresenter {
 
